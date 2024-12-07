69a4d81cb8057aa752a4d37c628fa4445556c776
SavinduMellawathanthrie <132599748+SavinduMellawathanthrie@users.noreply.github.com>
2024-12-07 11:05:05 +0530

    Initial commit

--- src/main/java/com/backend/SpringbootBackend/Configuration/SecurityIAM.java
+++ src/main/java/com/backend/SpringbootBackend/Configuration/SecurityIAM.java
@@ -2,9 +2,12 @@ package com.backend.SpringbootBackend.Configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityIAM {
@@ -15,9 +18,19 @@ public class SecurityIAM {
         UserDetails savindu = User.builder()
                 .username("Savindu")
                 .password("{noop}root")
-                .roles("ADMIN")
+                .roles("ADMIN") 
                 .build();
 
         return new InMemoryUserDetailsManager(savindu);
+    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer -> configurer
+                .requestMatchers(HttpMethod.GET, "api/students/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.POST, "api/students/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.PUT, "api/students/**").hasRole("ADMIN")
+                .requestMatchers(HttpMethod.DELETE, "api/students/**").hasRole("ADMIN")
+        );
+        return http.build();
     }
 }
