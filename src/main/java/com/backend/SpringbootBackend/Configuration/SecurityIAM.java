aea13deaef135e9a32701061168035a1925ff59c
SavinduMellawathanthrie <132599748+SavinduMellawathanthrie@users.noreply.github.com>
2024-12-08 22:27:54 +0530

    Initial commit

--- src/main/java/com/backend/SpringbootBackend/Configuration/SecurityIAM.java
+++ src/main/java/com/backend/SpringbootBackend/Configuration/SecurityIAM.java
@@ -3,13 +3,17 @@ package com.backend.SpringbootBackend.Configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityIAM {
 
     @Bean
@@ -26,16 +30,18 @@ public class SecurityIAM {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable()) // Disable CSRF for API-only applications
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers(HttpMethod.GET, "/api/students/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.POST, "/api/students/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.PUT, "/api/students/**").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.DELETE, "/api/students/**").hasRole("ADMIN")
-                ); // Simplified syntax for enabling HTTP Basic authentication
+                )
+                .httpBasic(Customizer.withDefaults()); // Enable HTTP Basic authentication
 
         return http.build();
     }
+
 
 
 
